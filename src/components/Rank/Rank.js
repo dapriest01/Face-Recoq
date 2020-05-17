0255f38e7627581f11f64a334ce4038eb6177978
Oluwafemi <dapriest01@gmail.com>
2020-05-17 10:40:31 +0100

    face recognition app

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+
+
+const Rank = () => {
+    return (
+       
+       <div>
+        <div className='white f3'>
+            {'Kilokanfamo, your rank is ...'}
+        </div>
+        <div className='white f2'>
+            {'Elite #2'}
+        </div>
+       
+       </div>
+    );
+}
+export default Rank;
