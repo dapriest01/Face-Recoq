0625ce2923bc3345c8445ed75b9e16d0c7aec2a8
Oluwafemi <dapriest01@gmail.com>
2020-05-17 21:27:40 +0100

    Updates

---
+++ precache-manifest.08db70988f6c9b546e168281f9c7c63c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e9c3cbaad8dafba253d9eaee7dda6f",
+    "url": "/face-recoq/index.html"
+  },
+  {
+    "revision": "80d4b78b95ffd1205cb2",
+    "url": "/face-recoq/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "24039646c38beae0eea3",
+    "url": "/face-recoq/static/css/main.5d34a722.chunk.css"
+  },
+  {
+    "revision": "80d4b78b95ffd1205cb2",
+    "url": "/face-recoq/static/js/2.135f82cb.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recoq/static/js/2.135f82cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24039646c38beae0eea3",
+    "url": "/face-recoq/static/js/main.3e4faf2b.chunk.js"
+  },
+  {
+    "revision": "1e6d575e12593657e418",
+    "url": "/face-recoq/static/js/runtime-main.066056e6.js"
+  }
+]);
